b1859be6b252b47a46d189500ded118adcf73e8e
Emma Rolsted <emma-k-r@hotmail.com>
2018-03-21 13:11:25 +0100

    Added integration project

---
+++ Microwave.Test.Integration/IntegrationTestCookCOntroller.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Interfaces;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Boundary;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    //class program
+    //{
+    //    static void Main(string[] args)
+    //    {
+
+    //    }
+    //}
+
+    [TestFixture]
+
+    class IntegrationTestCookCOntroller
+    {
+        private ICookController _cookController;
+        private IDisplay _display;
+        private IPowerTube _powerTube;
+        private ITimer _timer;
+        private IOutput _output;
+
+        [SetUp]
+        public void Setup()
+        {
+            _timer = Substitute.For<ITimer>();
+            _powerTube = Substitute.For<IPowerTube>();
+            _display = Substitute.For<IDisplay>();
+            _cookController = Substitute.For<ICookController>();
+            _output = Substitute.For<IOutput>();
+
+        }
+
+        [Test]
+        [TestCase(50,10)]
+        public void CookControllerStartup(int power, int time)
+        {
+            _cookController.StartCooking(power,time);
+            _timer.Start(time);
+            _powerTube.TurnOn(power);
+        }
+
+
+    }
+}
---
+++ Microwave.Test.Integration1/IntegrationTestCookCOntroller.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Interfaces;
+using MicrowaveOvenClasses.Controllers;
+
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+
+    [TestFixture]
+
+    class IntegrationTestCookCOntroller
+    {
+        private ICookController _cookController;
+        private IDisplay _display;
+        private IPowerTube _powerTube;
+        private ITimer _timer;
+        private IOutput _output;
+
+        [SetUp]
+        public void Setup()
+        {
+            _timer = Substitute.For<ITimer>();
+            _powerTube = Substitute.For<IPowerTube>();
+            _display = Substitute.For<IDisplay>();
+            //_cookController = Substitute.For<ICookController>();
+            _output = Substitute.For<IOutput>();
+            _cookController = new CookController(_timer,_display,_powerTube);
+
+        }
+
+        [Test]
+        [TestCase(50, 10)]
+        public void CookControllerStartup(int power, int time)
+        {
+            _cookController.StartCooking(power, time);
+
+            _powerTube.Received().TurnOn(power);
+            
+        }
+
+        //[Test]
+        //public void CookControllerStartTimer(int time)
+        //{
+        //    _timer.Start(time);
+        //}
+
+
+    }
+}
