eaa5d60b0ffa68703b06cb81478fa88dd1511c59
Susanne Olsen <susanne_olsen@live.dk>
2018-03-21 15:36:57 +0100

    Tried testing Door

---
+++ Microwave.Test.Integration/IntegrationTestDoor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IntegrationTestDoor
+    {
+        private IDoor _door;
+        private IUserInterface _userInterface;
+        private ILight _light;
+        private IDisplay _display;
+        private ICookController _cookController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _light = Substitute.For<ILight>();
+            _display = Substitute.For<IDisplay>();
+            _cookController = Substitute.For<ICookController>();
+            _userInterface = Substitute.For<IUserInterface>();
+            _door = new Door();
+        }
+
+        [Test]
+        public void DoorOpen()
+        {
+            _door.Open();
+            //_userInterface.Received().OnDoorOpened(this, EventArgs.Empty);
+            _light.Received().TurnOn();
+        }
+
+        [Test]
+        public void DoorClose()
+        {
+            //_door.Open();
+            _door.Close();
+            _userInterface.Received().OnDoorClosed(this, EventArgs.Empty);
+            //_light.Received().TurnOff();
+        }
+    }
+}
